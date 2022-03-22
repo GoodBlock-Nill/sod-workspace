8f8c4acfed6dbc95ae3c36179aa82e898f81eed9
NillYoo <nillbit.invest@gmail.com>
2022-03-23 03:18:36 +0900

    Header 작업중

---
+++ src/component/common/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'assets/css/common/common.css';
+import 'assets/css/common/header.css';
+
+const header = () => {
+  return (
+    <>
+      <header>
+        <div className='header-wrap'>
+          <div className='logo-wrap'>
+            <img src="" alt="" />
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default header;
